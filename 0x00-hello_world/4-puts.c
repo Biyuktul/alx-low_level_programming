418b3567a7b93f0fb4ff69b9d688202be2a8ba70
yonatan addis <yonatanaddis0257@gmail.com>
2022-06-30 11:17:03 -0700

    correcting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+#include <stdio.>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the text
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
