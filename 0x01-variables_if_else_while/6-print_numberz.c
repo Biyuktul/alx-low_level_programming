03efc0bdb576a95a3e24b41fddb93fce62914a55
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-03 06:26:17 -0700

    print number using putchar only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: the alphabet except e and q in lowercase, followed by a new line
+ **/
+
+int main(void)
+{
+char num;
+
+for (num = 0; num < 10; num++)
+{
+putchar((num % 10) + '0');
+}
+putchar('\n');
+
+return (0);
+}
