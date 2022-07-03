709ce739d2365a85e77eec0649b38875ce271e6b
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-03 06:04:49 -0700

    Print base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: the alphabet except e and q in lowercase, followed by a new line
+ **/
+
+int main(void)
+{
+char num;
+
+for (num = 0; num < 10; num++)
+{
+printf("%i", num);
+}
+
+return (0);
+}
