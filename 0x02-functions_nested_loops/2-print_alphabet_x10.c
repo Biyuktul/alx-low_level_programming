961d845c43555a461e887150229b2b47840931ae
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-05 11:01:19 -0700

    a-z 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Return: On success 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (i=0; i<10; i++)
+{
+for (i = 97; i < 123; i++)
+{
+  _putchar(i);
+}
+}
+_putchar('\n');
+}
