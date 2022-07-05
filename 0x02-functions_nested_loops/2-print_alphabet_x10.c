d10c6129cb7fd44d0221799455504ce9e3ce4429
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-05 11:03:33 -0700

    a-z 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,14 @@ void print_alphabet_x10(void)
 {
 int i;
 int j;
+
 for (j=0; j<10; j++)
 {
 for (i = 97; i < 123; i++)
 {
-  _putchar(i);
+_putchar(i);
 }
+_putchar('\n');
 }
 _putchar('\n');
 }
