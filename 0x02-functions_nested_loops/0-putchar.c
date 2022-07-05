5c3c855bb5d5e88b3afc584f98fdc25160df6da4
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-05 10:06:40 -0700

    including header file main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+char ch[] = "_putchar";
+int i;
+for (i = 0; i < 9; i++)
+{
+	_putchar(ch[i]);
+}
+_putchar('\n');
+
+return (0);
+}
