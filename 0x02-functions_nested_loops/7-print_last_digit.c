f18a77fcb55ffe8a91d9dbfc83c8ff8f54c19360
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-05 12:17:55 -0700

    print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @n: number to be checked
+ *
+ * main- Entry point
+ *
+ * Return: Always Last Digit
+ */
+
+int print_last_digit(int n)
+{
+int res;
+
+res = n % 10;
+return (res);
+}
+
