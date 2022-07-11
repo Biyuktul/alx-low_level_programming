d67f15fd43ca08645e95b3a598499e0911cd9268
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-11 07:03:17 -0700

    strcpy to copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * char *_strcpy - function that copies string pointed to by src
+ * @dest: destination
+ * @src: sorce
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != 0; i++)
+{
+	dest[i] = src[i];
+}
+return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+#My README
+**I'll use This repository to**
+1: practice on the repository
+2: as playground
