80489bb5e3306e99cfcab7a58c02a1ff29130de9
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-11 09:57:49 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - tells the length of string pointed to by s
+ * @s: the string tobe checked
+ *
+ * Return: Length of string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(a));
+}
