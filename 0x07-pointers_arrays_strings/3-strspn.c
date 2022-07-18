114f09c5193dbe2fc6ead36f092f7f09edb54372
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-18 09:13:15 -0700

    own implementation of strspn function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,20 +15,22 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int initial_length = 0;
 	unsigned int found_match, i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		found_match = 0;
 
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != 0; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				found_match = 1;
-				initial_length++;
+				break;
 			}
 		}
-		if (found_match == 0)
-			return (initial_length);
+		if (found_match)
+			initial_length++;
+		else
+			break;
 	}
 
 	return (initial_length);
