6ca9f7d281f85d08d5a746fe2907589955aeb599
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-18 06:11:46 -0700

    own implementation of memcpy function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,13 +11,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
-	if (src != NULL)
-	{
 		unsigned int i;
+
 		for (i = 0; i < n; i++)
 		{
 			dest[i] = src[i];
 		}
-	}
+
 	return (dest);
 }
