6dc3287ee2107a7360eba3556a4791d5815fdd94
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-20 02:16:15 -0700

    recursive implementation of sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that computes square root of a given number
+ * @n: number of type double
+ * Return: square root of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
