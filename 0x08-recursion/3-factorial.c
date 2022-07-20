f9ee51bc77ea2ef6b87e193128f22bf3a236ba37
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-20 01:21:45 -0700

    recursive implementation of factorial function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,14 @@
 /**
  * factorial - computes factorial of all positive numbers recursively
  * @n: number to be computed
- * Return: Factorial of given number
+ * Return: -1 if  n < 0, and 1 if n == 0 and return result if n < 1
  */
 
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	else if (n == 0)
+		return (1);
 	return (n * factorial(n - 1));
 }
