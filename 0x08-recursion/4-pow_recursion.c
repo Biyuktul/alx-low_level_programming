35f0d08a506ded3cfb9c0c7e980b1b2ce920b841
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-20 01:59:07 -0700

    recursive implementation of pow function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,11 +4,13 @@
  * _pow_recursion - computes power of a number
  * @x: base value
  * @y: a power raised to the base value
- * Return: 1 if y == 0 and computed value if y != 0
+ * Return: 1 if y == 0 and -1 if y <= and computed value if y != 0
  */
 
 int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+		return (-1);
 	if (y != 0)
 		return (x * _pow_recursion(x, y - 1));
 	else
