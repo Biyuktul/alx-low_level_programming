387cc2e34b7942c086a52109f6c0261573cb6a42
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-22 06:57:55 -0700

    enabeling our program to accept command line arguments /

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of command line argument passed
+ * @argv: array of command line arguments passed
+ * Return: Always (0) on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
