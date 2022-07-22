fccd3ab660273d82cb480a7261aba6e2a8f97732
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-22 07:39:05 -0700

    Bonus

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int change(int cents);
+
+/**
+ * main - Entry Point
+ * @argc: number of arguments
+ * @argv: array pointing to arguments
+ * Return: Always (0) at success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else if (argc < 0)
+	{
+		return (0);
+	}
+
+	printf("%d\n", change(atoi(argv[1])));
+	return (0);
+}
