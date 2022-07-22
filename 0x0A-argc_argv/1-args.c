388454bf8b8a929171f692e973259dafa9b8a85c
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-22 07:22:33 -0700

    printing number of command line argument passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,13 @@
  * Return: Always (0) on Success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
 	return (0);
 }
