899e281deba98fd57efbc5880dae1489c81dc408
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-25 07:41:03 -0700

    function that allocate specified size of memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,13 @@ char *create_array(unsigned int size, char c)
 	int i;
 
 	if (size == 0)
-		return (NULL);
+		return (0);
 
 	char *a = malloc(sizeof(char) * size);
 
-	if (a == NULL)
-		return (NULL);
-	else
-		for (i = 0; i < size; i++)
-			a[i] = c;
+	if (a == 0)
+		return (0);
+	for (i = 0; i < size; i++)
+		a[i] = c;
 	return (a);
 }
