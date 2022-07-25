f1db065e26ac0f5f9a6011a06f1a6b235498cae6
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-25 09:14:27 -0700

    function that copies a given string to newly dynamically allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that copies a string pointed to by str
+ *				to newly allocated memory.
+ * @str: pointer to the string.
+ * Return: on Success pointer to newly allocated memory or return
+ *			Null if allocated memory is insufficient or if passed
+ *			pointer to the string is empty.
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int len;
+	int i;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	if (str == 0)
+		return (0);
+	a = malloc(len);
+	i = 0;
+	while (str[i])
+	{
+		a[i] = str[i];
+		i++;
+	}
+	a[i] = '\0';
+	if (a == 0)
+		return (0);
+	return (a);
+	free(a);
+}
