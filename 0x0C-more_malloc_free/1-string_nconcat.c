8d3437a302cc9fbc67d2c52815b06f9050141ac3
yonatan addis <yonatanaddis0257@gmail.com>
2022-07-28 05:45:03 -0700

    function that concatenates a string pointed to by s1 to n characters of string pointed to by s2 into newly  allocated memory.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * string_nconcat - function that concatenates a string pointed to by s1 to
- *			n characters of string pointed to by s2 into newly  allocated memory.
+ * string_nconcat - function that concatenates a string pointed to by s1 to n
+ *		characters of string pointed to by s2 into newly  allocated memory.
  * @s1: pointer to th first string
  * @s2: pointer to the second string
  * @n: number of character to be concatenated
@@ -13,34 +14,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, k, len = n;
 	char *p;
+	unsigned int len = n, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (k = 0; s1[k]; k++)
+	for (i = 0; s1[i]; i++)
 		len++;
 
+	p = malloc(sizeof(char) * (len + 1));
 
-        p = malloc(sizeof(char) * (len + 1));
-
-	if  (p == NULL)
+	if (p == NULL)
 		return (NULL);
 
+	len = 0;
+
 	for (i = 0; s1[i]; i++)
-	{
-		p[i] = s1[i];
-	}
+		p[len++] = s1[i];
 
-	for (j = 0; s2[j] && j < n; j++, i++)
-	{
-		p[i] = s2[j];
-	}
+	for (i = 0; s2[i] && i < n; i++)
+		p[len++] = s2[i];
 
-	p[i] = '\0';
+	p[len] = '\0';
 
 	return (p);
 }
