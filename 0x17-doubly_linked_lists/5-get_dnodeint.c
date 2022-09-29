8236f8c03efa0a12dbe8b186151562297e8494c2
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-09-29 07:57:33 -0700

    adding function to get n'th element from a list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds a node at specfied index
+ * @head: pointer to the start of the list
+ * @index: position of the node to be searched
+ * Return: The node at pos index or NULL if doesn't exist in the list
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i;
+
+	i = 0;
+	while (head != NULL || i != index)
+	{
+		head = head->next;
+		i++;
+	}
+	if (head == NULL)
+		return (NULL);
+
+	return (head->n);
+}
