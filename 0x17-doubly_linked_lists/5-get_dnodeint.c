450e39ac3cf4e44d8d97735b76f17b335a02a0bf
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-09-29 08:13:33 -0700

    Bug fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,13 +12,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 
 	i = 0;
-	while (head != NULL || i != index)
+	while (head != NULL)
 	{
-		head = head->next;
+		if (i == index)
+			return (head);
 		i++;
+		head = head-> next;
 	}
 	if (head == NULL)
 		return (NULL);
-
-	return (head);
 }
