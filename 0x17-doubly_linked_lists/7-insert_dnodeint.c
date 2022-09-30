ab4664cc728717135e3fab3ab27d69ef2ef3f082
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-09-30 00:40:36 -0700

    Hot Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (temp == NULL)
-		return (add_dnodeint_end(h,n));
+		return (add_dnodeint_end(h, n));
 
 	newNode->next = temp;
 	newNode->prev = temp->prev;
