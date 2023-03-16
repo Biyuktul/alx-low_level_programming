a3579010366876e00d3371971af6580917735444
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-16 19:42:49 +0300

    adding file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,12 +23,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	else
-	{
+	
 		
 		new_node->next = *head;
 		new_node->prev = (*head)->prev;
 		*head = new_node;
 		return (new_node);
-	}
 }
+
