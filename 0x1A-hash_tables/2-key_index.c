9680481798cee25d273048faa2dfbb2826810260
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-07 13:03:59 +0300

    bug fix

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - a function that gives the index of a key.
+ * @key:
+ * @size:
+ * Return:
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+}
