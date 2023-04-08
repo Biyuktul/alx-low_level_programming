a7bec21a8f5561b53615591cacb7e79b78c7e8c1
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-08 19:10:46 +0300

    modifying

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,13 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 	new_node->key = (char *)key;
-	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+	new_node->value = (char *)value;
 	new_node->next = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
 
