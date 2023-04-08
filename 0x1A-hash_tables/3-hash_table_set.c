b46e7b8d369834dbe123bfb7620003dae64c5f6c
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-08 19:04:06 +0300

    modifying

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,13 +15,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (strlen(key) == 0 || strlen(value) == 0)
+	if (key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
 
