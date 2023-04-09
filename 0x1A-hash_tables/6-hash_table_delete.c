d13e67311bd4ab60905cdf3ed0e643ddbba4a751
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-09 16:39:33 +0300

    modifying

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hash table.
+ * Return: Always void.
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+        if (ht == NULL)
+		return (0);
+        
+}
