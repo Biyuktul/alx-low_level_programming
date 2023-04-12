e054b2023334ce0a55d47144cd958de702c339ba
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-13 00:48:47 +0300

    bug fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,8 +39,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
 	}
 	return (1);
 }
