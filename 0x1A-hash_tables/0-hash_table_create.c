5e52f96a943a9285195bf1e2e55302417d6d5e49
Yonatan Addis <=>
2023-07-11 15:49:25 +0300

    refactoring

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,25 +4,27 @@
  * hash_table_create - function to create hash table data structure
  * @size: the size for the hash table
  * Return: pointer to the newly created hash table
-*/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
 	unsigned long int i;
+	if (!size)
+		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
+	ht->array = malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
+	ht->size = size;
+
 	return (ht);
 }
